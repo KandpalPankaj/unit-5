6d15b1bf613036be7266e48eb2d6d06af2ba331a
pankajnandi <pkandpal28@gmail.com>
2022-04-19 08:19:58 +0530

    form updated

--- sprint2/forms/class/src/App.jsx
+++ sprint2/forms/class/src/App.jsx
@@ -1,34 +1,35 @@
-import { useEffect, useRef, useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [counter, setCounter] = useState(0)
-  const counterRef =useRef(null)
- 
-useEffect(()=>{
-  start();
-},[])
-  const start=()=>{
-    counterRef.current=setInterval(()=>{
-      setCounter((counter)=>{
-        return counter+1;
-      })
-    },100)
-  }
+  const [counter, setCounter] = useState(0);
+  const counterRef = useRef(null);
+
+  useEffect(() => {
+    start();
+  }, []);
+  const start = () => {
+    counterRef.current = setInterval(() => {
+      setCounter((counter) => {
+        return counter + 1;
+      });
+    }, 100);
+  };
   return (
     <div className="App">
       <h3>Count:{counter}</h3>
-      <button onClick={()=>(
-        clearInterval(counterRef.current)
-  )}>Pause</button>
+      <button onClick={() => clearInterval(counterRef.current)}>Pause</button>
       <button onClick={start}>Start</button>
-      <button onClick={()=>{
-        setCounter(0)
-        clearInterval(counterRef.current)
-      }}>Reset</button>
+      <button
+        onClick={() => {
+          setCounter(0);
+          clearInterval(counterRef.current);
+        }}
+      >
+        Reset
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
