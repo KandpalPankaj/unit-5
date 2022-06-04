e0528655abcd8fa92342a8d651c02c5f8f8ac7a1
pankajnandi <pkandpal28@gmail.com>
2022-06-04 20:47:14 +0530

    done

---
+++ unit6/WEB SCRAPING/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5500;
+const bodyParser = require("body-parser");
+const scrapers = require("./scrapers");
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // disabled for security on local
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.listen(port, () =>
+  console.log(`listening at http://localhost:${port}`)
+);
+
+app.post("/something", async (req, res) => {
+  console.log("something endpoint", req.body);
+  const jobData = await scrapers.scrapeChannel(req.body.customURL, req.body.techInput);
+  res.send(jobData);
+});
