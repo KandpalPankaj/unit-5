c0aaa867c70f7bb5d49ca59d9e3da854871775f4
pankajnandi <pkandpal28@gmail.com>
2022-06-06 23:44:38 +0530

    update

---
+++ unit6/entity/backend/src/models/product.model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const productSchema = new mongoose.Schema({
+    image:{ type: String, required: true } ,
+    name: { type: String, required: true },
+    main_price: { type: Number, required: true },
+    discount: { type: Number, required: true },
+    type: { type: String, required: true },
+    color: { type: String, required: true }
+})
+
+module.exports = mongoose.model("product", productSchema)
---
+++ unit6/entity/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { Product } from "./components/Products";
+import "./App.css";
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Product />
+    </div>
+  );
+}
+
+export default App;
