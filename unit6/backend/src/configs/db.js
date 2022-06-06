56d698f7ba9f63875d78cad650ff4acdeb1cab50
pankajnandi <pkandpal28@gmail.com>
2022-06-06 17:09:24 +0530

    folders ready

---
+++ unit6/backend/src/configs/db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const connect = () =>{
+    return mongoose.connect("mongodb+srv://pankajKandpal:pankand@cluster0.g4xc3.mongodb.net/lastSprint?retryWrites=true&w=majority")
+} 
+
+module.exports = connect;
