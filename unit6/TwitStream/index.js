59c9ece4a67f4c077ceaa05042a6b68764bd08b4
pankajnandi <pkandpal28@gmail.com>
2022-06-08 11:38:04 +0530

    upadte

--- unit6/TwitStream/index.js
+++ unit6/TwitStream/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const socketIo = require("socket.io");
 const needle = require("needle");
 
-
 const http = require("http");
 const path = require("path");
 const TOKEN = process.env.TWITTER_BEARER_TOKEN;
@@ -17,10 +16,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./", "index.html"));
 });
 
-
 const rulesURL = "https://api.twitter.com/2/tweets/search/stream/rules";
 
-const streamURL ="https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id";
+const streamURL =
+  "https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id";
 
 const rules = [{ value: "art" }];
 
@@ -30,16 +29,12 @@ async function getRules() {
       Authorization: `Bearer ${TOKEN}`,
     },
   });
-
-  console.log(response.body);
   return response.body;
 }
-
 async function setRules() {
   const data = {
     add: rules,
   };
-
   const response = await needle("post", rulesURL, data, {
     headers: {
       "content-type": "application/json",
@@ -49,44 +44,30 @@ async function setRules() {
 
   return response.body;
 }
-
-
-
 async function deleteRules(rules) {
   if (!Array.isArray(rules.data)) {
     return null;
   }
-
   const ids = rules.data.map((rule) => rule.id);
-
   const data = {
     delete: {
       ids: ids,
     },
   };
-
-
-
-
   const response = await needle("post", rulesURL, data, {
     headers: {
       "content-type": "application/json",
       Authorization: `Bearer ${TOKEN}`,
     },
   });
-
   return response.body;
 }
-
 function streamTweets(socket) {
   const stream = needle.get(streamURL, {
     headers: {
       Authorization: `Bearer ${TOKEN}`,
     },
   });
-
-
-
   stream.on("data", (data) => {
     try {
       const json = JSON.parse(data);
@@ -94,30 +75,19 @@ function streamTweets(socket) {
       socket.emit("tweet", json);
     } catch (error) {}
   });
-
   return stream;
 }
-
-io.on
-("connection", async () => {
-  console.log("Client connected...");
-
+io.on("connection", async () => {
   let currentRules;
-
   try {
     currentRules = await getRules();
-
     await deleteRules(currentRules);
-
     await setRules();
   } catch (error) {
-    console.error(error);
     process.exit(1);
   }
 
   streamTweets(io);
 });
 
-
-
-server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+server.listen(PORT, () => console.log(`Pankaj listening on port ${PORT}`));
