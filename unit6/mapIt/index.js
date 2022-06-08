de7773c696258980d9434287072c574f67a0b4e8
pankajnandi <pkandpal28@gmail.com>
2022-06-08 11:27:04 +0530

    update

--- unit6/mapIt/index.js
+++ unit6/mapIt/index.js
@@ -1,23 +1,29 @@
 function initMap() {
-    let arr = [{ id: 1, m: 1, position: { lat: 20.5937, lng: 78.9629 }, title: "india", },
-    { id: 2, m: 2, position: { lat: 30.3165, lng:78.0322, }, title: "Lucknow", },
-        { id: 3, m: 3, position: { lat: 30.8467, lng: 80.7462, }, title: "Unknown", },
-        { id: 4, m: 4, position: { lat: 29.8467, lng: 80.8462, }, title: "Unknown", }]
-    let map;
+  let arr = [
+    { id: 1, m: 1, position: { lat: 20.5937, lng: 78.9629 }, title: "india" },
+    {
+      id: 2,
+      m: 2,
+      position: { lat: 30.3165, lng: 78.0322 },
+      title: "Dehradun",
+    },
+    { id: 3, m: 3, position: { lat: 30.8467, lng: 80.7462 }, title: "Unknown" },
+    { id: 4, m: 4, position: { lat: 29.8467, lng: 80.8462 }, title: "Unknown" },
+  ];
+  let map;
 
-    let mapOption = {
-        center: { lat: 20.5937, lng: 78.9629 },
-        zoom: 8
-    }
-    map = new google.maps.Map(document.getElementById("map"), mapOption);
+  let mapOption = {
+    center: { lat: 20.5937, lng: 78.9629 },
+    zoom: 5,
+  };
+  map = new google.maps.Map(document.getElementById("map"), mapOption);
 
-    arr.map((e) => {
-        e.id = {
-            position: e.position,
-            title: e.title,
-
-        }
-        e.m = new google.maps.Marker(e.id)
-        e.m.setMap(map)
-    })
+  arr.map((el) => {
+    el.id = {
+      position: el.position,
+      title: el.title,
+    };
+    el.m = new google.maps.Marker(el.id);
+    el.m.setMap(map);
+  });
 }
